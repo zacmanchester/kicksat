32dc25e675b29b0d5a978917152500eb15c16dbf
Zac <Zac@wdyn-128-102-239-37.arc.nasa.gov>
2013-01-31 15:33:24 -0800

    Tweaked RadioData demo

--- DevelopmentKit/Energia/RadioData/RadioData.ino
+++ DevelopmentKit/Energia/RadioData/RadioData.ino
@@ -40,7 +40,10 @@ void setup() {
 void loop() {
   //Blink LED while transmitter is on
   digitalWrite(5, HIGH);
-  m_radio.transmit("Hello World", 11);
+  m_radio.transmit("H", 1);
+  m_radio.transmit("i", 1);
+  m_radio.transmit("!", 1);
+  m_radio.transmit(" ",1);
   digitalWrite(5, LOW);
   
   //Wait a second
